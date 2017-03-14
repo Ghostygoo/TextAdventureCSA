c07bf299b0eae3b450109a4ae1aea49448bbac5f
NightDeath77 <epacwilliam@gmail.com>
2017-03-14 09:02:42 -0500

    Added
    stuff

--- TextAdventure.java
+++ TextAdventure.java
@@ -9,10 +9,12 @@
 import java.util.Scanner;
 public class TextAdventure
 {
-    public static void main( String[] args ){
+   Scanner in = new Scanner(System.in);
+    public void main(){
      
         TextAdventure call = new TextAdventure();
-        Scanner in = new Scanner(System.in);
+        
+        
         System.out.println("Welcome to a relatively simple text adventure");
         System.out.println("  ");
         System.out.println("You are returning from high school and you are at a crossroad,");
@@ -20,45 +22,34 @@ public class TextAdventure
         String statement = in.nextLine();
         statement = statement.toLowerCase();
      
-         if (statement.contains("forwards")){
-             System.out.println("  ");
-            System.out.println("You reach a crime scene and you see a high grade bank being robbed,");
-            System.out.println("what do you do?(Attack armed robber, avoid situation)");
-            
-            
+        if (statement.contains("forwards")){
+            System.out.println("  ");
+            forwardsAtStart();
 
-     
-        
-            
-            String statement1 = in.nextLine();
-            statement = statement1.toLowerCase();
-                if (statement1.contains("attack")){
-                    System.out.println("  ");
-                    System.out.println("You get shot in the chest and start to bleed,");
-                    System.out.println("and you are paralized from the neck down,");
-                    System.out.println("but the robber has been distracted long enough to be arrested,");
-                    System.out.println("yet his wingman has gotten away with the cash while the police didn't notice");
-                    System.out.println("what do you do?(Call for an officer to help you,");
-                    System.out.println("tell one of the units of the remaining theif on the run)");
         }
-            if (avoid3.contains("avoid the situation")){
-                System.out.println("One of the members of the gang notices you so he shot you 10 times,");
-                System.out.println("twice in the side, thrice in the chest, twice in the hip,");
-                System.out.println("twice in the stomach, and once in the head, killing you.");  
-                
-          
-        
-       
-            }
         else if (statement.contains("left")){
             System.out.println("You are at a dead end");
+            System.out.println("You go back where you started.");
+            System.out.println("Which direction to you go in? ('forwards' or 'right')");
             
+            String statementseventhree = in.nextLine();
+            statementseventhree = statementseventhree.toLowerCase();
+            
+            if (statementseventhree.contains("forwards")){
+                System.out.println("  ");
+                forwardsAtStart();
+            }
+            else if (statementseventhree.contains("right")){
+                System.out.println("  ");
+                rightAtStart();
+            }
         }
         else if (statement.contains("right")){
-    
+            System.out.println("This ");
         }    
         else{
-            System.out.println("this is the else");
+            System.out.println("sorry, that is not an option.");
+            main();
         }   
 
 
@@ -173,5 +164,75 @@ public class TextAdventure
       } */
 
    }
+
+   public void attackBankRobber(){
+       System.out.println("You get shot in the chest and start to bleed,");
+       System.out.println("and you are paralized from the neck down,");
+       System.out.println("but the robber has been distracted long enough to be arrested,");
+       System.out.println("yet his wingman has gotten away with the cash while the police didn't notice");
+       System.out.println("what do you do?(Call for an officer to help you,");
+       System.out.println("tell one of the units of the remaining theif on the run)");
+    }
+    public void avoidSituation(){
+       System.out.println("You get taken hostage by the initials robber wingman,");
+       System.out.println("and now you have been tied up and put up with the other captives,");
+       System.out.println("what do you do?(roll over and help the others, help yourself, do nothing)");           
+    }
+    public void forwardsAtStart(){
+        System.out.println("You reach a crime scene and you see a high grade bank being robbed,");
+        System.out.println("what do you do?('attack' armed robber, 'avoid' situation)");
+            
+        String statement1 = in.nextLine();
+        statement1 = statement1.toLowerCase();
+        if (statement1.contains("attack")){
+            System.out.println("  ");
+            attackBankRobber();
+        }
+        if (statement1.contains("avoid")){
+           System.out.println("  ");
+           avoidSituation();
+        }
+    }
+    public void rightAtStart(){
+        System.out.println("You go to an alley way, you could either go back to where you started,");
+        System.out.println("or continue forwards, which do you choose?(Go 'back', 'continue' forwards)");
+        
+        String statementseventy = in.nextLine();
+        statementseventy = statementseventy.toLowerCase();
+        
+        if (statementseventy.contains("back")){
+            main();
+        }
+        if (statementseventy.contains("continue")){
+            System.out.println("  ");
+            System.out.println("You reach the other side of the alley way,");
+            System.out.println("and then you see group of people trying to kidnap someone,");
+            System.out.println("what do you do?(help the 'kidnappers', help the 'kidnapped', 'avoid' the situation)");
+            
+            String statementeight = in.nextLine();
+            statementeight = statementeight.toLowerCase();
+            
+            if (statementeight.contains("kidnappers")){
+                helpKidnappers();
+            }
+            if (statementeight.contains("kidnapped")){
+                helpKidnapped();
+            }
+            if (statementeight.contains("avoid")){
+                avoidingKidnap();
+            }
+        }
+        
+    }
+    public void helpKidnappers(){
+        
+    }
+    public void helpKidnapped(){
+        
+    }
+    public void avoidingKidnap(){
+        
+    }
+    
 }
 
